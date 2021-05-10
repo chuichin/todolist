95293337c01f118d5190365e964d6ac3bcd3c5e7
chuichin <chuichin.code@gmail.com>
2021-05-10 15:00:10 +0800

    replaced / post

--- app.js
+++ app.js
@@ -21,14 +21,19 @@ app.get("/", function(req,res){
     res.render("list", {listTitle : day, newListItem: items})
 })
 
- 
+app.post("/", function(req, res){
+    const item = req.body.newItem;
+
+    if (req.body.list ===  "Work"){
+        workItems.push(item);
+        res.redirect('/work')
+    } else {
+        items.push(item)
+        res.redirect('/')
+    }
+})
 
 app.get('/work', function(req, res){
     res.render("list", {listTitle: "Work List", newListItem: workItems})
 })
 
-app.post("/work",function(req, res){
-    let item = req.body.newItem;
-    workItems.push(item)
-    res.redirect("/work")
-})
